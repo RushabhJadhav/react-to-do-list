b7362b5614cd6109e79084f93b86a1cc3b0898d3
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2023-10-01 14:43:23 +0530

    added use state

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import AddTodo from "./add-button/AddTodo";
 import "./Input.css";
 
 const Input = () => {
+    const [todo, setTodo] = useState("")
     const handleInput = (e) => {
-        console.log(e.target.value)
+        setTodo(e.target.value)
+        // console.log(e.target.value)
     }
     return (
         <div className="input-cont">
-            <input type="text" placeholder="Add your todo" onChange={handleInput} />
-            <AddTodo />
+            <input type="text" placeholder="Add your todo" value={todo} onChange={handleInput} />
+            <AddTodo todo={todo} />
         </div>
     )
 }
--- src/components/input/add-button/AddTodo.js
+++ src/components/input/add-button/AddTodo.js
@@ -1,13 +1,13 @@
 import "./AddTodo.css";
 
-const AddTodo = () => {
+const AddTodo = ({ todo }) => {
     const handleClick = (e) => {
         e.preventDefault()
-        console.log('add button clicked')
+        console.log(todo)
     }
     return (
         <>
-            <button className="add-button" onClick={handleClick}>Add</button>
+            <button className="add-button" type="submit" onClick={handleClick}>Add</button>
         </>
     )
 }
