3ad783701f37b5ab43ef9ada738e758acb742638
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2023-10-05 17:58:04 +0530

    style changes

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -9,7 +9,7 @@ const Input = ({ todoLists, setTodoLists }) => {
     }
     return (
         <div className="input-cont">
-            <input type="text" placeholder="Add your todo" value={todo} onChange={handleInput} />
+            <input type="text" placeholder="Add your todo..." value={todo} onChange={handleInput} />
             <AddTodo todo={todo} todoLists={todoLists} setTodoLists={setTodoLists} />
         </div>
     )
